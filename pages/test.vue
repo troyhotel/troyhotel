98076ac43cc8a421007992e2de07dcd360f64a3b
troyhotel <troy.hotel.krd@yandex.ru>
2025-10-17 17:00:19 +0300

    тестю видео

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+                  <PlayerVideo :src="videoSrc" class="banquet-service__video" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PlayerVideo from '~/components/ui/PlayerVideo.vue'
+const videoSrc = '/spa/sample-5s.mp4'  // путь к вашему видео
+</script>
+
+<style>
+
+</style>
