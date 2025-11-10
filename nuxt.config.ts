25153659bdfad5fe3e7d6a7a8010d6fbf2fdf3d1
troyhotel <troy.hotel.krd@yandex.ru>
2025-11-10 18:49:54 +0300

    настройка метрики

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -175,16 +175,37 @@ export default defineNuxtConfig({
         {
           type: "text/javascript",
           innerHTML: `
-            var _tmr = window._tmr || (window._tmr = []);
-            _tmr.push({id: "3538859", type: "pageView", start: (new Date()).getTime()});
-            (function (d, w, id) {
-              if (d.getElementById(id)) return;
-              var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-              ts.src = "https://top-fwz1.mail.ru/js/code.js";
-              var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-              if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-            })(document, window, "tmr-code");
-          `,
+          var _tmr = window._tmr || (window._tmr = []);
+          _tmr.push({id: "3538859", type: "pageView", start: (new Date()).getTime()});
+          (function (d, w, id) {
+            if (d.getElementById(id)) return;
+            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+            ts.src = "https://top-fwz1.mail.ru/js/code.js";
+            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+          })(document, window, "tmr-code");
+        `,
+        },
+
+        // Яндекс Метрика
+        {
+          type: "text/javascript",
+          innerHTML: `
+          (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+          ym(53290438, 'init', {
+            webvisor:true,
+            clickmap:true,
+            ecommerce:"dataLayer",
+            accurateTrackBounce:true,
+            trackLinks:true
+          });
+        `,
         },
         // JSON-LD схема отеля
         {
@@ -214,8 +235,13 @@ export default defineNuxtConfig({
         },
       ],
       noscript: [
+        // Mail.ru фолбэк
         {
           innerHTML: `<div><img src="https://top-fwz1.mail.ru/counter?id=3538859;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div>`,
+        },
+        // Яндекс Метрика фолбэк
+        {
+          innerHTML: `<div><img src="https://mc.yandex.ru/watch/53290438" style="position:absolute;left:-9999px;" alt="" /></div>`,
         },
       ],
     },
